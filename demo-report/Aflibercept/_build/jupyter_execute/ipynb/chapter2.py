679a249ac0e7318e69d2c3a4548de5e8da58b6f4
deepthought <partrita@gmail.com>
2022-04-29 16:40:02 +0900

    initial commit for template

---
+++ demo-report/Aflibercept/_build/jupyter_execute/ipynb/chapter2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 2. Potential sites of chemical modification
+# 
+# An initial scan of the antibody sequences is presented based purely upon sequence. If a
+# structural analysis was also requested, this section should be used in conjunction with the
+# molecular surface analysis described in a subsequent section. Any of the sites listed below
+# could be candidates for further consideration if the molecular surface analysis shows that they
+# are significantly exposed on the surface of the antibody, increasing their propensity for chemical
+# modification. The canonical sequence analysis is also helpful here, since each of these sites
+# can also be considered in the context of their frequency of occurrence within the canonical
+# library of homologous sequences.
+# 
+# ## tryptophan and methionine residues in CDRs
+# 
+# The indole ring of Tryptophan (W) is prone to oxidation, especially when exposed to UV.
+# Tryptophan residues in exposed positions in the CDRs, can affect the solubility of an antibody
+# and lead to non-specific antigen binding.
+# Methionine (M) residues are prone to oxidation through the formation of a sulfoxide, and can
+# also become irreversibly oxidized. This oxidation can be accelerated by exposure to metal ions
+# (e.g. from the wall of a stainless steel fermentation vessel).
+# 
+
+# ## potential deamidation positions
+# 
+# Asparagine (N) and glutamine (Q) residues are particularly prone to deamidation when they are
+# followed in the sequence by amino acids with smaller side chains, that leave the intervening
+# peptide group more exposed. Deamidation proceeds much more quickly if the susceptible
+# amino acid is followed by a small, flexible residue such as glycine whose low steric hindrance
+# leaves the peptide group open for attack.
+# 
+# 
+# Search patterns: ASN/GLN-ALA/GLY/SER/THR
+
+# ## potential o-linked glycosylation sites
+# 
+# The O-linked glycosylation of serine and threonine residues seems to be particularly sensitive to
+# the presence of one or more proline residues in their vicinity in the sequence, particularly in the2
+# -1 and +3 positions.
+# 
+# 
+# Search patterns: PRO-SER/THR, SER/THR-X-X-PRO
+
+# ## potential n-linked glycosylation sites
+# Search patterns: ASN-X-SER/THR
